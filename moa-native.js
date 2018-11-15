235938903b2edd6f0766d3a02ac81a1a78eb1fe5
pan he <you@example.com>
2018-11-15 17:32:22 +0800

    韩国国hhh

--- moa-native.js
+++ moa-native.js
@@ -17,37 +17,35 @@
 			  
 			  }
 		},
-		handleBackApp2(){
-			moa.handleBackApp1()
-		},
+	
 		//自动登录
-		handleAutoLogin(){
-			alert("333")
-			// var res={
-			// 	empName:"欧阳红2",
-   //    			img_url:"http://192.168.117.151/group1/M00/00/01/wKh1l1tICa2AWBUwAAGOQMQZWts825.jpg",
-   //    			companyId:"38",
-			// 	deptId:"39",
-			// 	deptName:"产品研发运营部",
-			// 	userid:"114"
-			// }
-			// return Promise.resolve(res);
-			buttonClickTest({
-	             result: function(data) {
-	             // demo.innerHTML = data["test"];
-	              alert(data["test"])
-	              }
-	            })
-			 // var code = {testString: "goBack",}
+		// handleAutoLogin(){
+		// 	alert("333")
+		// 	// var res={
+		// 	// 	empName:"欧阳红2",
+  //  //    			img_url:"http://192.168.117.151/group1/M00/00/01/wKh1l1tICa2AWBUwAAGOQMQZWts825.jpg",
+  //  //    			companyId:"38",
+		// 	// 	deptId:"39",
+		// 	// 	deptName:"产品研发运营部",
+		// 	// 	userid:"114"
+		// 	// }
+		// 	// return Promise.resolve(res);
+		// 	buttonClickTest({
+	 //             result: function(data) {
+	 //             // demo.innerHTML = data["test"];
+	 //              alert(data["test"])
+	 //              }
+	 //            })
+		// 	 // var code = {testString: "goBack",}
 
-			 // window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
-		},
-	    function buttonClickTest(testData) {
-	        var result = testData.result
-	        //此处不能直接将回调函数传给iOS需要将回调函数转成字符串,其他的保持不变即可
-	        testData.result = result.toString()
-	        window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
-	    },
+		// 	 // window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
+		// },
+	    // function buttonClickTest(testData) {
+	    //     var result = testData.result
+	    //     //此处不能直接将回调函数传给iOS需要将回调函数转成字符串,其他的保持不变即可
+	    //     testData.result = result.toString()
+	    //     window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
+	    // },
 
 		//查看附件
 		handleViewAttach(url){
