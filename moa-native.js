7efbb2b0a4577ffebbf8767a2ade4f90eab6a59a
lanbosm <lanbosm@gmail.com>
2018-11-15 14:29:48 +0800

    sss

---
+++ moa-native.js
@@ -0,0 +1,45 @@
+!function(win){
+
+	var moa={
+		//返回app
+		handleBackApp(){
+			alert("返回app")
+		},
+		//自动登录
+		handleAutoLogin(){
+			var res={
+				empName:"欧阳红",
+      			img_url:"http://192.168.117.151/group1/M00/00/01/wKh1l1tICa2AWBUwAAGOQMQZWts825.jpg",
+      			companyId:"38",
+				deptId:"39",
+				deptName:"产品研发运营部",
+				userid:"114"
+			}
+			return Promise.resolve(res);
+		},
+		//查看附件
+		handleViewAttach(url){
+			alert("查看附件"+url)
+		},
+		//选人
+		handleSelectMember(uid){
+			
+
+			var res=[
+				{empName:"欧阳红的部下1",userid:"115"},
+				{empName:"欧阳红的部下2",userid:"116"}
+
+			]
+			return Promise.resolve(res);
+				
+			
+		}
+
+
+	}
+
+	win.$moa=moa;
+
+	
+
+}(window)
