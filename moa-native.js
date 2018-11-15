7c14d68bb918a03997fe5eb8e63f3f044a77428a
pan he <you@example.com>
2018-11-15 18:14:56 +0800

    韩国国极极极9000

--- moa-native.js
+++ moa-native.js
@@ -6,23 +6,20 @@
  
 	var moa={
 		//返回app
-
 		handleBackApp(){
 			var code = {testString: "goBack",}
-           
            if(isiOS){ 
             	window.webkit.messageHandlers.buttonClickGoBack.postMessage(code)
 			  }else{
 			   	window.moa1.handleBackApp1();
 			   	alert("2222")
-			  
 			  }
 		},
 		
 
 	
 		//自动登录
-		// handleAutoLogin(){
+		 handleAutoLogin(){
 		// 	alert("333")
 		// 	// var res={
 		// 	// 	empName:"欧阳红2",
@@ -33,22 +30,20 @@
 		// 	// 	userid:"114"
 		// 	// }
 		// 	// return Promise.resolve(res);
-		// 	buttonClickTest({
-	 //             result: function(data) {
-	 //             // demo.innerHTML = data["test"];
-	 //              alert(data["test"])
-	 //              }
-	 //            })
-		// 	 // var code = {testString: "goBack",}
-
-		// 	 // window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
-		// },
-	    //  buttonClickTest(testData) {
-	    //     var result = testData.result
-	    //     //此处不能直接将回调函数传给iOS需要将回调函数转成字符串,其他的保持不变即可
-	    //     testData.result = result.toString()
-	    //     window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
-	    // },
+			buttonClickTest({
+	             result: function(data) {
+	             // demo.innerHTML = data["test"];
+	              alert(data["test"])
+	              }
+	            })
+			
+		},
+	     buttonClickTest(testData) {
+	        var result = testData.result
+	        //此处不能直接将回调函数传给iOS需要将回调函数转成字符串,其他的保持不变即可
+	        testData.result = result.toString()
+	        window.webkit.messageHandlers.buttonClickGetUserInfo.postMessage(testData);
+	    },
 
 		//查看附件
 		handleViewAttach(url){
